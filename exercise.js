ad56147f6c376c4fb378a2e01f8da75327eea23a
Remedeae <sanna.m.blomdahl@gmail.com>
2024-10-14 21:01:53 +0200

    exercises added

---
+++ exercise.js
@@ -0,0 +1,23 @@
+//Exercise 1
+let text ="Hello World!";
+console.log(text);
+
+//Exercise 2
+let Name ="Sanna-Maya";
+let age =33;
+let faveColor ="Green";
+
+console.log(`${Name} has seen ${age} ${faveColor} springs.`);
+
+//Exercise 3
+let x=36;
+let y=45;
+
+x += y;     //36+45 = 81 = x
+x --;       //81-1 = 80 = x
+x *= y;     //80*45 = 3600 = x
+x /= 3;     //3600/3 = 1200 = x
+x %= 9      //1200/9 = 133 rest 3 -> x=3
+x ++;       //3+1 = 4 = x
+
+console.log(x);
